8c090874406b41038b92d31a4e92950eb8d918cc
Kennnedu <r.kenda94@gmail.com>
2017-02-06 01:16:01 +0300

    remove lib folder

--- lib/check_one_time.rb
+++
@@ -1,7 +0,0 @@
-# when user update phrases
-# Example call CheckOneTime.new.call(@phrase.histories.last.user.id, @user.id)
-class CheckOneTime
-  def call(last_user_id, current_user_id)
-    raise 'Error' if last_user_id == current_user_id
-  end
-end
--- lib/check_word.rb
+++
@@ -1,7 +0,0 @@
-# when user update phrases
-# Example call CheckWord.new.call(params[:phrase][:name])
-class CheckWord
-  def call(word)
-    raise 'ArgumentError' if /(\.){1}/ =~ word || /(,|!|:|;|\?|-){2}/ =~ word || /[\s]/ =~ word || word.empty?
-  end
-end
