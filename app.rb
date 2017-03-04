578ead6a39fa705785050c194f4586213901c565
Kennnedu <ro.kenda@mail.ru>
2017-03-04 21:12:24 +0300

    some changes

--- app.rb
+++ app.rb
@@ -10,28 +10,31 @@ set :database, { adapter: 'postgresql',
   encoding: 'unicode', database: 'your_database_name', pool: 2,
   username: 'roman', password: 'password'}
 
-class Application < Sinatra::Base
-  get '/' do
-    haml :index
-  end
+after do
+  ActiveRecord::Base.clear_active_connections!
+end 
 
-  get '/phrases' do
-    { phrases: Phrase.all }.to_json
-  end
-
-  post '/phrase' do
-    params_json = JSON.parse(request.body.read)
-    { phrase: Phrase.create(params_json['phrase']) }.to_json
-  end
-
-  put '/phrase/:id' do
-    params_json = JSON.parse(request.body.read)
-    @phrase = Phrase.find(params[:id])
-    @phrase.update(name: params_json['phrase'])
-    { phrase: @phrase.name }.to_json
-  end
-
-  delete '/phrase/:id' do
-    { message: Phrase.destroy(params[:id]) }.to_json
-  end
+get '/' do
+  haml :index
 end
+
+get '/phrases' do
+  { phrases: Phrase.all }.to_json
+end
+
+post '/phrase' do
+  params_json = JSON.parse(request.body.read)
+  { phrase: Phrase.create(params_json['phrase']) }.to_json
+end
+
+put '/phrase/:id' do
+  params_json = JSON.parse(request.body.read)
+  @phrase = Phrase.find(params[:id])
+  @phrase.update(name: params_json['phrase'])
+  { phrase: @phrase.name }.to_json
+end
+
+delete '/phrase/:id' do
+  { message: Phrase.destroy(params[:id]) }.to_json
+end
+
