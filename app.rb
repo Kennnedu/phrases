782433ebb2dfb4e08c000c3a0fc4716f2550aa6f
Kennnedu <r.kenda94@gmail.com>
2018-06-15 22:30:57 +0300

    add components; provide base functionality

--- app.rb
+++ app.rb
@@ -23,38 +23,45 @@ end
 
 get '/' do
   authorize
-  @phrases = Phrase.recent_updated
   @username = session[:username]
   erb :index, layout: :application
 end
 
-post '/create_phrase' do
+get '/phrases' do
+  authorize
+  @phrases = Phrase.recent_updated
+  json user: { id: session[:user_id], username: session[:username] },
+    phrases: @phrases.as_json(only: [:id, :current_state, :updated_at])
+end
+
+post '/phrase' do
   authorize
   @phrase = Phrase.new(current_state: params[:beginning_phrase])
   if @phrase.save
-    json id: @phrase.id, phrase: @phrase.current_state
+    json phrase: @phrase.as_json(only: [:id, :current_state, :updated_at])
   else
     status 404
-    json message: @phrase.errors.messages
+    json message: @phrase.errors.full_messages.join(' ')
   end
 end
 
-post '/create_word' do
+post '/word' do
   authorize
-  @word = Word.new(user_id: session[:user_id], phrase_id: params[:word][:phrase_id].to_i,
-    word: params[:word][:word])
+  @word = Word.new(user_id: session[:user_id], phrase_id: params[:phrase_id].to_i,
+    word: params[:word])
   if @word.save
-    json id: @word.id, phrase: @word.word
+    json phrase: @word.phrase.as_json(only: [:id, :current_state, :updated_at])
   else
     status 404
-    json message: @word.errors.messages
+    json message: @word.errors.full_messages.join(' ')
   end
 end
 
 get '/history/:phrase_id' do
   authorize
-  @phrases = Phrase.eager_load(:words).find(params[:phrase_id])
-  json phrases: @phrases
+  @words = Phrase.find(params[:phrase_id]).words.includes(:user)
+    .as_json(only: [:id, :word, :previous_phrase, :created_at], include: { user: { only: :username }})
+  json words: @words
 end
 
 get '/sign_up' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,26 +1,88 @@
-Vue.component('navigation-bar', {
-  template: "#navigation-bar",
-  props: ['username'],
+Vue.component('history-entry', {
+  template: "#history-entry",
+  props: ['historyEntry'],
+  computed: {
+    createdDate: function(){
+      return new Date(this.historyEntry.created_at).toLocaleString();
+    }
+  }
+});
+
+Vue.component('phrase-entry', {
+  template: "#phrase-entry",
+  props: ['phrase', 'activePhraseId'],
   data: function(){
     return {
-      phrase: {
-        beginningPhrase: ''
-      }
+      newWord: ""
+    }
+  },
+  computed: {
+    isOpenedPhrase: function(){
+      return this.activePhraseId == this.phrase.id
+    }
+  },
+  methods: {
+    addNewWord: function(){
+      phraseEntry = this
+      axios({
+        url: '/word',
+        method: 'post',
+        params: {
+          word: phraseEntry.newWord,
+          phrase_id: phraseEntry.phrase.id
+        }
+      })
+      .then(function(response){
+        phraseEntry.newWord = ""
+        phraseEntry.phrase.current_state = response.data.phrase.current_state
+        phraseEntry.phrase.updated_at = response.data.phrase.updated_at
+        phraseEntry.$emit('notify', 'You successfully add new word!', 'success')
+      })
+      .catch(function(response){
+        console.log(response.response);
+        phraseEntry.newWord = ""
+        phraseEntry.displayEditing = false
+        phraseEntry.$emit('notify', response.response.data.message, 'danger')
+      });
+    },
+    loadPhraseInfo: function(){
+      phraseEntry = this
+      phraseEntry.displayEditing = !phraseEntry.displayEditing
+      if(!phraseEntry.displayEditing) {
+        return phraseEntry.$emit('save-history', [], 0);
+      } 
+      axios('/history/' + phraseEntry.phrase.id).then(function(response){
+        phraseEntry.$emit('save-history', response.data.words, phraseEntry.phrase.id);
+      });
+    }
+  }
+});
+
+Vue.component('navigation-bar', {  template: "#navigation-bar",
+  props: ['username', 'phrases'],
+  data: function(){
+    return {
+      beginningPhrase: ''
     }
   },
   methods: {
     createPhrase: function(){
+      navigationComponent = this
       axios({
-        url: '/create_phrase',
+        url: '/phrase',
         method: 'post',
         params:{
-          beginning_phrase: this.phrase.beginningPhrase
+          beginning_phrase: this.beginningPhrase
         }
       })
       .then(function(response){
-        // debugger;
+        navigationComponent.beginning_phrase = ''
+        navigationComponent.phrases.push(response.data.phrase)
+        navigationComponent.$emit('notify', 'You successfully add new phrase!', 'success')
       }).catch(function(response){
-        debugger;
+        console.log(response.response);
+        navigationComponent.beginning_phrase = ''
+        navigationComponent.$emit('notify', response.response.data.message, 'danger')
       });
     }
   }
@@ -29,10 +91,53 @@ Vue.component('navigation-bar', {
 var app = new Vue({
   el: "#app",
   data: {
-    user: {
-      id: '',
-      username: '',
-      phrases: {}
+    currentUser: {},
+    phrases: [],
+    notification: {
+      message: '',
+      type: ''
+    },
+    history: [],
+    activePhraseId: 0
+  },
+  created: function(){
+    app = this;
+    axios('/phrases').then(function(response){
+      app.phrases = response.data.phrases
+      app.currentUser = response.data.user
+    })
+  },
+  computed: {
+    sortedPhrases: function(){
+      return this.phrases.sort(function(a, b){
+        var dateA = new Date(a.updated_at)
+        var dateB = new Date(b.updated_at)
+        return dateB - dateA
+      })
+    }
+  },
+  watch: {
+    notification: { 
+      handler: function(){
+        if(this.notification.message.length) {
+          app = this
+          setTimeout(function(){
+            app.notification.message = ''
+            app.notification.type = ''
+          }, 10000);
+        }
+      },
+      deep: true
+    }
+  },
+  methods: {
+    changeNotification: function(message, type){
+      this.notification.message = message
+      this.notification.type = type
+    },
+    saveHistory: function(history, activePhraseId){
+      this.activePhraseId = activePhraseId
+      this.history = history.reverse()
     }
   }
 });
